4aaccfeedb23cf96986278c18891ac5c5538f63c
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2023-08-15 23:15:02 -0400

    added meta info, added title to projects section

--- src/components/ProjectsGrid.jsx
+++ src/components/ProjectsGrid.jsx
@@ -4,8 +4,10 @@ import projects from "../projects.json"
 export default function ProjectsGrid() {
   return(
     <section id="projects" class="h-full text-gray-600 body-font bg-gray-950">
-      <div class="container px-5 py-24 mx-auto h-fit">
+      <h1 className="flex justify-center text-9xl font-primary">PROJECTS</h1>
+      <div class="container px-5 py-24 mx-auto h-fit">      
         <div class="flex flex-wrap justify-around">
+          
           {
             projects.map(project =>
               <ProjectCard key={project.id} project={project}/>
