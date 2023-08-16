843cb89c9441dc6f7f56c3c6e3234183a89f7c48
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2023-08-15 23:15:27 -0400

    fixed color for project title

--- src/components/ProjectsGrid.jsx
+++ src/components/ProjectsGrid.jsx
@@ -4,7 +4,7 @@ import projects from "../projects.json"
 export default function ProjectsGrid() {
   return(
     <section id="projects" class="h-full text-gray-600 body-font bg-gray-950">
-      <h1 className="flex justify-center text-9xl font-primary">PROJECTS</h1>
+      <h1 className="flex justify-center text-9xl text-violet-500 font-primary">PROJECTS</h1>
       <div class="container px-5 py-24 mx-auto h-fit">      
         <div class="flex flex-wrap justify-around">
           
