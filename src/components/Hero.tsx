bac35599164e134940eba622df9cf080a9fd017f
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2024-05-17 22:32:08 -0400

    added logos to hero buttons, added button for github

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
 import TypingDescription from './TypingDescription';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faNewspaper } from '@fortawesome/free-regular-svg-icons';
 
 const Hero: React.FC = () => {
   return (
@@ -10,8 +13,9 @@ const Hero: React.FC = () => {
           <h1 className="text-5xl font-bold">Hey, I&apos;m Lorenzo!</h1>
           <TypingDescription />
           <div className='flex justify-center md:block'>
-            <a href='/docs/mokwa-lorenzo-resume.pdf' rel='noopener noreferrer' target='_blank' className="btn text-white mr-5">Resumé</a>
-            <a href='https://www.linkedin.com/in/lomokwa/' rel='noopener noreferrer' target='_blank' className="btn text-white">LinkedIn</a>
+            <a href='/docs/mokwa-lorenzo-resume.pdf' rel='noopener noreferrer' target='_blank' className="btn text-white mr-5"><FontAwesomeIcon size='2x' icon={faNewspaper} />Resumé</a>
+            <a href='https://www.linkedin.com/in/lomokwa/' rel='noopener noreferrer' target='_blank' className="btn text-white mr-5"><FontAwesomeIcon size='2x' icon={faLinkedin}/>LinkedIn</a>
+            <a href='https://www.github.com/lomokwa' rel='noopener noreferrer' target='_blank' className='btn text-white'><FontAwesomeIcon size='2x' icon={faGithub}/>Github</a>
           </div>
         </div>
       </div>
