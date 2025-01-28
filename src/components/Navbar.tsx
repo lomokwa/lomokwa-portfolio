2d54ac00c05ade63649573ae4db1384120ce4be9
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2025-01-28 03:48:46 -0500

    feat: added experience timeline

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,15 +11,16 @@ export default function Navbar() {
             </div>
             <ul tabIndex={0} className="menu dropdown-content z-[1] p-2 shadow bg-base-100 rounded-box w-52 mt-4">
               <li><a href="#tech">Tech</a></li> 
+              <li><a href="#experience">Experience</a></li>
               <li><a href="#projects">Projects</a></li>
-              <li><a href="#about">About</a></li>
+              {/* <li><a href="#about">About</a></li> */}
               <li><a href='/docs/mokwa-lorenzo-resume.pdf' rel='noopener noreferrer' target='_blank'>Resumé</a></li>
               <li><a href="https://www.linkedin.com/in/lomokwa/" rel='noopener noreferrer' target='_blank'>LinkedIn</a></li>
             </ul>
           </div>
         </div>
         <div className="flex-1">
-          <a className="btn btn-ghost text-xl">lomokwa</a>
+          <a className="px-[1rem] text-xl">lomokwa</a>
         </div>
       </div>
     </div>
---
+++ src/components/Timeline.tsx
@@ -0,0 +1,137 @@
+import React from "react";
+
+const Timeline: React.FC = () => {
+  return (
+    <div id="experience" className="mx-auto max-w-[1024px]">
+      <h2 className="text-4xl font-bold text-center mt-20">Experience</h2>
+      <ul className="mt-5 mx-20 timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+        <li>
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end">
+            <time className="font-mono italic">Jun 2022 – On Hold</time>
+            <div className="text-lg font-black">Florida Atlantic University – B.S. Computer Science</div>
+            Pursuing a B.S. in Computer Science, developing a strong foundation in programming and computer science principles.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-end md:mb-10">
+            <time className="font-mono italic">Jun 2023 – Aug 2023</time>
+            <div className="text-lg font-black">Boca Code – Software Engineering Bootcamp</div>
+            Completed an intensive software engineering bootcamp, gaining hands-on experience with full-stack development, including React, Node.js, and other industry-standard technologies.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end">
+            <time className="font-mono italic">Aug 2023 – Dec 2023</time>
+            <div className="text-lg font-black">Camlann Games – Software Engineering Internship</div>
+            Contributed to the development of a dashboard application, collaborating on both frontend and backend tasks, and ensuring seamless integration for studios, publishers, and stakeholders.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-end md:mb-10">
+            <time className="font-mono italic">Nov 2023 – Jun 2024</time>
+            <div className="text-lg font-black">Venancio USA – Freelance Software Developer</div>
+            Developed custom software solutions to optimize warehouse management, improving inventory tracking and order fulfillment, while providing technical support and training to staff for smooth software adoption.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-start mb-10 md:text-end">
+            <time className="font-mono italic">Jun 2024 – Sep 2024</time>
+            <div className="text-lg font-black">Site Impact – Jr. Software Developer</div>
+            Collaborated on the development of a B2B marketing application for managing and placing marketing campaigns. Implemented custom integrations with services like Facebook and Google, and contributed to in-house library development.
+          </div>
+          <hr />
+        </li>
+        <li>
+          <hr />
+          <div className="timeline-middle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="h-5 w-5">
+              <path
+                fillRule="evenodd"
+                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                clipRule="evenodd" />
+            </svg>
+          </div>
+          <div className="timeline-end md:mb-10">
+            <time className="font-mono italic">Dec 2023 – Present</time>
+            <div className="text-lg font-black">Insite Web – Jr. Software Developer</div>
+            Currently leading the development of client-facing applications, optimizing UI/UX integration, and collaborating with stakeholders to ensure the successful delivery of software solutions.
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Timeline;
