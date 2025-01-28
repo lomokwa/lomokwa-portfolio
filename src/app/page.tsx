3dd030e0ca607f647afde540d6b3058a06347cd7
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2025-01-28 03:49:14 -0500

    feat: added timeline component, disabled startup animation temporarily

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,16 @@ import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import StartupAnimation from "@/components/StartupAnimation";
 import TechStack from "@/components/TechStack";
+import Timeline from "@/components/Timeline";
 
 export default function Home() {
   return (
     <main className="bg-gray-950">
-      <StartupAnimation />
+      {/* <StartupAnimation /> */}
       <Navbar />
       <Hero />
       <TechStack />
+      <Timeline />
       <Projects />
       <Footer />
     </main>
